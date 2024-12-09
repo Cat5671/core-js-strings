89e59f498a0df92aec9141ff7f19f3a53f3a664f
Cat5671 <152156513+Cat5671@users.noreply.github.com>
2024-12-09 15:57:47 +0300

    feat: add task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -189,8 +189,11 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  let sum = 0;
+  if (typeof str != 'string') return 0;
+  for (let i = 0; i < str.length; i++) sum += str.charCodeAt(i);
+  return sum;
 }
 
 /**
@@ -204,8 +207,8 @@ function sumOfCodes(/* str */) {
  *   startsWith('Hello World', 'World') => false
  *   startsWith('Hello World', 'Hello') => true
  */
-function startsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function startsWith(str, substr) {
+  return str.startsWith(substr);
 }
 
 /**
@@ -219,8 +222,8 @@ function startsWith(/* str, substr */) {
  *   endsWith('Hello World', 'World') => true
  *   endsWith('Hello World', 'Hello') => false
  */
-function endsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function endsWith(str, substr) {
+  return str.endsWith(substr);
 }
 
 /**
